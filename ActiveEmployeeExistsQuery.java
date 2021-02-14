c8ae0fec187e69665497b29a2b9824b10b43bf81
cjgrisco <78048051+cjgrisco@users.noreply.github.com>
2021-02-14 12:35:54 -0600

    Add files via upload

---
+++ ActiveEmployeeExistsQuery.java
@@ -0,0 +1,10 @@
+package edu.uark.registerapp.commands.employees;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
